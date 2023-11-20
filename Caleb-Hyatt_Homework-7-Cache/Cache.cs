32585972bd33b456767c039bf2e2764845f9dddb
calebhyatt <calebphyatt@gmail.com>
2023-11-19 19:38:31 -0500

    Add project files.

---
+++ Caleb-Hyatt_Homework-7-Cache/Cache.cs
@@ -0,0 +1,132 @@
+﻿namespace Caleb_Hyatt_Homework_7_Cache
+{
+    public class CacheData
+    {
+        public string data { get; set; }
+        public int order { get; set; }
+        public DateTime lastUsed { get; set; } = DateTime.Now;
+        public int accessCount { get; set; } = 0;
+    }
+
+    internal class Cache
+    {
+        public Dictionary<string, CacheData> data;
+        public int size { get; set; }
+        public int order = 0;
+
+        public Cache(int size)
+        {
+            this.size = size;
+            this.data = new Dictionary<string, CacheData>();
+        }
+
+        public void ClearCache()
+        {
+            foreach (string key in this.data.Keys)
+            {
+                this.data.Remove(key);
+            }
+        }
+
+        /**
+         * --- FIFO ---
+         * This just returns the cache, ordered by order.
+         */
+        public string Get(string key)
+        {
+            // If cache hit
+            if (this.data.ContainsKey(key))
+            {
+                // Say so and return the value (data)
+                Console.WriteLine($"Cache hit for {key}");
+                return this.data[key].data;
+            }
+            // If cache miss
+            else
+            {
+                // Say so
+                Console.WriteLine($"Cache miss for {key}");
+
+
+                // songString is a proof of work. I changed it to be one line/variable
+                // This portion was done in class
+                var songString = string.Join("", key.Reverse().ToArray());
+
+                if (this.data.Count >= this.size)
+                {
+                    // The OrderBy sort is changed to sort by the order property
+                    var firstIn = this.data.OrderBy(x => x.Value.order).First().Key;
+                    Console.WriteLine($"[FIFO] Evicting item with key {firstIn}");
+                    this.data.Remove(firstIn);
+                }
+
+                // Changed the increment to be cleaner
+                this.order++;
+                this.data[key] = new CacheData() { data = songString, order = this.order };
+                return songString;
+            }
+        }
+
+        /**
+         * --- LRU ---
+         * Same appraoch as the Get method. The difference is we set the lastUsed property to the current time with DateTime.Now and it's sorted by lastUsed.
+         */
+        public string GetLRU(string key)
+        {
+            if (this.data.ContainsKey(key))
+            {
+                this.data[key].lastUsed = DateTime.Now;
+                Console.WriteLine($"Cache hit for {key}");
+                return this.data[key].data;
+            }
+            else
+            {
+                Console.WriteLine($"Cache miss for {key}");
+
+                var songString = string.Join("", key.Reverse().ToArray());
+
+                if (this.data.Count >= this.size)
+                {
+                    var leastRecent = this.data.OrderBy(x => x.Value.lastUsed).First().Key;
+                    Console.WriteLine($"[LRU] Evicting item with key {leastRecent}");
+                    this.data.Remove(leastRecent);
+                }
+
+                this.order++;
+                this.data[key] = new CacheData() { data = songString, order = this.order };
+                return songString;
+            }
+        }
+
+        /**
+         * --- LFU ---
+         * Same appraoch as LRU except we increment accessCount by 1 instead of changing lastUsed and sort by that property.
+         */
+        public string GetLFU(string key)
+        {
+            if (this.data.ContainsKey(key))
+            {
+                this.data[key].accessCount++;
+                Console.WriteLine($"Cache hit for {key}");
+                return this.data[key].data;
+            }
+            else
+            {
+                Console.WriteLine($"Cache miss for {key}");
+
+                var songString = string.Join("", key.Reverse().ToArray());
+
+                if (this.data.Count >= this.size)
+                {
+                    var leastFrequent = this.data.OrderBy(x => x.Value.accessCount).First().Key;
+                    Console.WriteLine($"[LFU] Evicting item with key {leastFrequent}");
+                    this.data.Remove(leastFrequent);
+                }
+
+                this.order++;
+                this.data[key] = new CacheData() { data = songString, order = this.order };
+                return songString;
+            }
+        }
+    }
+}
---
+++ Caleb-Hyatt_Homework-7-Cache/Program.cs
@@ -0,0 +1,45 @@
+﻿using Caleb_Hyatt_Homework_7_Cache;
+
+var cache = new Cache(2);
+
+Console.WriteLine("--- LRU ---\n");
+
+/**
+ * A real-world example of an LRU cache is the cache used by a web browser to store recently visited web pages. When you visit a website, your browser caches a copy of the page and its resources, such as images and scripts. When you return to the same website, the web browser can load the page from the cache rather than downloading it from the internet, which can be much faster.
+ * However, because the cache has a limited capacity, it cannot store an infinite number of web pages. When the cache is full, the web browser must remove some of the previously cached web pages to make room for new ones. In this case, the web browser determines which web pages to evict using an LRU cache algorithm. Web pages that have not been accessed in a long time are removed first, while pages that have been accessed recently are kept in the cache.
+ */
+
+var lru1 = cache.GetLRU("1");
+Console.WriteLine(lru1);
+var lru2 = cache.GetLRU("2");
+Console.WriteLine(lru2);
+var lru3 = cache.GetLRU("3");
+Console.WriteLine(lru3);
+var lru4 = cache.GetLRU("2");
+Console.WriteLine(lru4);
+var lru5 = cache.GetLRU("4");
+Console.WriteLine(lru5);
+
+Console.WriteLine("\n--- LFU ---");
+cache.ClearCache();
+
+/**
+ * Font caching in web browsers is an example of an LFU cache in action. When a web page is loaded, the browser may need to download fonts that the page requires. The web browser can cache downloaded fonts in memory or on disk to speed up subsequent page loads.
+ * An LFU cache can be used to prioritize the caching of the most frequently used fonts while evicting fonts that are used less frequently to make room for new fonts. When cached fonts are used, their frequency count is incremented, and when the cache is full, the font with the lowest frequency count is evicted.
+ * Font caching with an LFU cache can improve page load times and reduce the number of font downloads required because frequently used fonts can be served from the cache rather than being downloaded each time they are needed. This can enhance the overall user experience while also reducing the load on web servers.
+ */
+
+var lfu1 = cache.GetLFU("1");
+Console.WriteLine(lfu1);
+var lfu2 = cache.GetLFU("1");
+Console.WriteLine(lfu2);
+var lfu3 = cache.GetLFU("2");
+Console.WriteLine(lfu3);
+var lfu4 = cache.GetLFU("2");
+Console.WriteLine(lfu4);
+var lfu5 = cache.GetLFU("2");
+Console.WriteLine(lfu5);
+var lfu6 = cache.GetLFU("3");
+Console.WriteLine(lfu6);
+var lfu7 = cache.GetLFU("4");
+Console.WriteLine(lfu7);
